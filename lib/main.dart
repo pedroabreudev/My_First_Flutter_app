0395d23c4c945f8ef938b653d51b3b736ef67b8a
Pedro Henrique <pedroabreu.dev@gmail.com>
2022-05-03 11:30:54 -0400

    Finalizando First App

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,40 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    theme: ThemeData(primarySwatch: Colors.orange),
-    home: Scaffold(
+      theme: ThemeData(primarySwatch: Colors.purple), home: HomePage()));
+}
+
+// ignore: use_key_in_widget_constructors
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var count = 0;
+
+  void increment() {
+    count++;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: const Text("Meu Primeiro App"),
       ),
-      body: Container(
-        color: Colors.purple,
-        child: Center(
-            child: Container(
-          height: 200,
-          width: 200,
-          color: Colors.green,
-        )),
+      body: Center(
+          child: Text(
+        "CONTADOR\n$count",
+        textAlign: TextAlign.center,
+      )),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          increment();
+        },
       ),
-    ),
-  ));
+    );
+  }
 }
